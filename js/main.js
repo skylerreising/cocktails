cd5c57445a7f136a28e9e6d1aca5906f915004e9
Skyler Reising <skyler.reising@gmail.com>
2023-09-09 17:00:24 -0500

    Lets try this again

---
+++ js/main.js
@@ -0,0 +1,31 @@
+//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+document.querySelector(".getCocktail").addEventListener("click", getDrink)
+function getDrink(){
+    let drink = document.querySelector('input').value
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data.drinks)
+      document.querySelector("h2").innerText = data.drinks[0].strDrink
+      document.querySelector("img").src = data.drinks[0].strDrinkThumb
+      document.querySelector("h3").innerText = data.drinks[0].strInstructions
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+}
+
+document.querySelector(".randomButton").addEventListener("click", getRandoDrink)
+function getRandoDrink(){
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+    document.querySelector("h2").innerText = data.drinks[0].strDrink
+      document.querySelector("img").src = data.drinks[0].strDrinkThumb
+      document.querySelector("h3").innerText = data.drinks[0].strInstructions
+  })
+  .catch(err => {
+    console.log(`error ${err}`)
+  })
+}
